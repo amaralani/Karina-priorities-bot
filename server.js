fd97d306366f0cb4348b8c5555c9c9ea6470e452
a.maralani <a.maralani@karinaco.com>
2018-12-05 14:44:06 +0330

    V1.0

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ const { Markup } = Telegraf;
 console.log(Markup);
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.help((ctx) => ctx.reply("لطفا جهت شروع مجدد عبارت \"new\" یا \"جدید\" را وارد کنید."));
 bot.on('callback_query', (ctx,next) => {react(ctx,ctx.callbackQuery.data);ctx.answerCbQuery().then()});
 
 bot.on('message', (ctx) => {
-    react(ctx,ctx.message);
+    if(ctx.message === "new" || ctx.message ==="جدید"){
+        begin(ctx);
+    }else {
+        ctx.reply("لطفا جهت شروع مجدد عبارت \"new\" یا \"جدید\" را وارد کنید.")
+    }
 });
 bot.startPolling();
 
