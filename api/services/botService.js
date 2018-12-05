01e82e29aa26958d41b879cf9c51f69bc6e2906f
a.maralani <a.maralani@karinaco.com>
2018-12-05 12:00:15 +0330

    Initial Commit

---
+++ api/services/botService.js
@@ -0,0 +1,32 @@
+module.exports = {
+    begin: function (ctx) {
+        const inlineMessageKeyboard = Markup.inlineKeyboard([
+            {text: 'بله👍', callback_data: 'main-function'},
+            {text: 'خیر👎', callback_data: 'low'}
+        ]).extra();
+        ctx.telegram.sendMessage(ctx.from.id, "آیا کار مورد نظر جزء عملکردهای اصلی شرکت است؟",inlineMessageKeyboard);
+    },
+    askForCoupling: function (ctx) {
+        const inlineMessageKeyboard = Markup.inlineKeyboard([
+            {text: 'بله👍', callback_data: 'has-coupling'},
+            {text: 'خیر👎', callback_data: 'medium'}
+        ]).extra();
+        ctx.telegram.sendMessage(ctx.from.id, "آیا برروی موارد دیگر تاثیر میگذارد؟", inlineMessageKeyboard);
+    },
+
+    askForDeadline: function (ctx) {
+        const inlineMessageKeyboard = Markup.inlineKeyboard([
+            {text: 'بله👍', callback_data: 'affects-deadline'},
+            {text: 'خیر👎', callback_data: 'high'}
+        ]).extra();
+        ctx.telegram.sendMessage(ctx.from.id, "آیا  باعث نرسیدن به مهلت سررسید می شود؟", inlineMessageKeyboard);
+    },
+
+    askForSeverFault: function (ctx) {
+        const inlineMessageKeyboard = Markup.inlineKeyboard([
+            {text: 'بله👍', callback_data: 'blocker'},
+            {text: 'خیر👎', callback_data: 'high'}
+        ]).extra();
+        ctx.telegram.sendMessage(ctx.from.id, "آیا باعث مختل شدن کار کاربر می‌ شود؟", inlineMessageKeyboard);
+    }
+};
---
+++ server.js
@@ -0,0 +1,42 @@
+const Telegraf = require('telegraf');
+var service = require("./api/services/botService")
+const bot = new Telegraf("1234");
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.on('message', (ctx) => {
+    switch (ctx.message) {
+        case "low" :
+            ctx.reply("low");
+            break;
+        case "medium" :
+            ctx.reply("medium");
+            break;
+        case "high" :
+            ctx.reply("high");
+            break;
+        case "highest" :
+            ctx.reply("highest");
+            break;
+        case "blocker" :
+            ctx.reply("blocker");
+            break;
+        case "is-main-function" :
+            ctx.reply("Medium");
+            break;
+        case "main-function" :
+            service.askForCoupling(ctx);
+            break;
+        case "has-coupling" :
+            service.askForDeadline(ctx);
+            break;
+        case "affects-deadline" :
+            service.askForSeverFault(ctx);
+            break;
+        default :
+            service.begin(ctx);
+    }
+});
+bot.startPolling();
+
+
